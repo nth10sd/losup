cdc06a28a88f7fd8f5b304d1419bb2dbf5fc56fc
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-05-21 18:40:44 -0700

    Bump Mgk to v23.0

--- losup/scripts/op6t_dl_newest_LOS.sh
+++ losup/scripts/op6t_dl_newest_LOS.sh
@@ -14,7 +14,7 @@ aria2c --force-sequential=true --max-connection-per-server=5 --split=10 \
     https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-10.0-nano-$2.zip \
     https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-10.0-nano-$2.zip.md5 \
     https://downloads.sourceforge.net/project/mauronofrio-twrp/Fajita/twrp-3.3.1-32-fajita-installer-mauronofrio.zip \
-    https://github.com/topjohnwu/Magisk/releases/download/v22.0/Magisk-v22.0.apk
+    https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk
 popd;
 echo "Press any key once all aria2c instances have completed successfully, else press Ctrl-C to abort.";
 read;
@@ -23,7 +23,7 @@ read;
 LOS_FILENAME="lineage-17.1-$1-nightly-fajita-signed";
 TWRP_FILENAME="twrp-3.3.1-32-fajita-installer-mauronofrio";
 OGA_FILENAME="open_gapps-arm64-10.0-nano-$2";
-MAG_FILENAME="Magisk-v22.0";
+MAG_FILENAME="Magisk-v23.0";
 
 echo "Verifying checksums...";
 aria2c https://mirrorbits.lineageos.org/full/fajita/$1/lineage-17.1-$1-nightly-fajita-signed.zip?sha256
@@ -31,7 +31,7 @@ shasum -c $HOME/$LOS_FILENAME.1.zip;  # Not a zip file, it is the SHA256 hash
 echo "e6c36b0a9ddc092e03038e5a4178ed8ce2206089e1d1067b0b0af10ba51ccfa2 *$TWRP_FILENAME.zip" > $HOME/$TWRP_FILENAME.zip.sha256;
 shasum -c $HOME/$TWRP_FILENAME.zip.sha256;
 md5sum -c $HOME/$OGA_FILENAME.zip.md5;
-echo "dcb0a1f84f629fe31a3b39bd222241a93f74fb5caf48c521e5a5aca4459e1bf1 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
+echo "079bc3a7f67413cb12509d1b811958dd7e7c7fb593d3f0507954fea72077b524 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
 shasum -c $HOME/$MAG_FILENAME.apk.sha256;
 
 echo "Removing checksum files...";
