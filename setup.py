8c1851636978903b95e0249c9585974d1d866e09
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-05-23 17:13:20 -0700

    Bump to Python 3.9

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         # "<dependency>",
     ],
     extras_require=EXTRAS,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     zip_safe=False,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
@@ -84,7 +84,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Testing",
         "Topic :: Utilities",
