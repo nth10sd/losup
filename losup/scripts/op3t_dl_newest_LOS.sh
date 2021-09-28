f2317a88a831eb0ef9b0721e77f8960aab27e018
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-09-28 16:24:35 -0700

    Bump Mgk to v23.0

--- losup/scripts/op3t_dl_newest_LOS.sh
+++ losup/scripts/op3t_dl_newest_LOS.sh
@@ -17,7 +17,7 @@ aria2c --force-sequential=true --max-connection-per-server=5 --split=10 \
     https://mirrorbits.lineageos.org/full/oneplus3/$1/lineage-17.1-$1-nightly-oneplus3-signed.zip \
     https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-10.0-nano-$2.zip \
     https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-10.0-nano-$2.zip.md5 \
-    https://github.com/topjohnwu/Magisk/releases/download/v22.0/Magisk-v22.0.apk;
+    https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk;
 popd;
 echo "Press any key once all aria2c instances have completed successfully, else press Ctrl-C to abort.";
 read;
@@ -26,7 +26,7 @@ read;
 LOS_FILENAME="lineage-17.1-$1-nightly-oneplus3-signed";
 TWRP_FILENAME="twrp-3.4.0-0-oneplus3";
 OGA_FILENAME="open_gapps-arm64-10.0-nano-$2";
-MAG_FILENAME="Magisk-v22.0";
+MAG_FILENAME="Magisk-v23.0";
 
 echo "Verifying checksums...";
 aria2c https://mirrorbits.lineageos.org/full/oneplus3/$1/lineage-17.1-$1-nightly-oneplus3-signed.zip?sha256
@@ -34,7 +34,7 @@ shasum -c $HOME/$LOS_FILENAME.1.zip;  # Not a zip file, it is the SHA256 hash
 aria2c https://dl.twrp.me/oneplus3/twrp-3.4.0-0-oneplus3.img.sha256
 sudo shasum -c $HOME/$TWRP_FILENAME.img.sha256;
 md5sum -c $HOME/$OGA_FILENAME.zip.md5;
-echo "dcb0a1f84f629fe31a3b39bd222241a93f74fb5caf48c521e5a5aca4459e1bf1 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
+echo "079bc3a7f67413cb12509d1b811958dd7e7c7fb593d3f0507954fea72077b524 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
 shasum -c $HOME/$MAG_FILENAME.apk.sha256;
 
 echo "Removing checksum files...";
