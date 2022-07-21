89394c500459e4143ebae4392f6c2fb4d21342a4
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-07-20 18:17:44 -0700

    Remove all references to obsolete methods and update links

--- losup/scripts/op3t_dl_newest_LOS.sh
+++ losup/scripts/op3t_dl_newest_LOS.sh
@@ -2,7 +2,7 @@
 echo;
 echo "This script is for OnePlus 3T.";
 echo;
-echo "Sample usage: <script> <datestamp of LineageOS build> <datestamp of OpenGApps build>";
+echo "Sample usage: <script> <datestamp of LineageOS build>";
 echo 'Make sure to have run "pkg install aria2 perl tsu" beforehand!';
 echo 'OP3T: Make sure to have downloaded https://dl.twrp.me/oneplus3/twrp-3.4.0-0-oneplus3.img beforehand!';
 echo "Press any key once all aria2c commands finish!";
@@ -14,10 +14,7 @@ echo "Downloading files...";
 pushd $HOME;
 # TWRP redirects break downloading from TWRP. Just download into $HOME beforehand
 aria2c --force-sequential=true --max-connection-per-server=5 --split=10 \
-    https://mirrorbits.lineageos.org/full/oneplus3/$1/lineage-18.1-$1-nightly-oneplus3-signed.zip \
-    https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-11.0-nano-$2.zip \
-    https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-11.0-nano-$2.zip.md5 \
-    https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk;
+    https://mirrorbits.lineageos.org/full/oneplus3/$1/lineage-18.1-$1-nightly-oneplus3-signed.zip;
 popd;
 echo "Press any key once all aria2c instances have completed successfully, else press Ctrl-C to abort.";
 read;
@@ -25,20 +22,15 @@ read;
 # Set variable names
 LOS_FILENAME="lineage-18.1-$1-nightly-oneplus3-signed";
 TWRP_FILENAME="twrp-3.4.0-0-oneplus3";
-OGA_FILENAME="open_gapps-arm64-11.0-nano-$2";
-MAG_FILENAME="Magisk-v23.0";
 
 echo "Verifying checksums...";
 aria2c https://mirrorbits.lineageos.org/full/oneplus3/$1/lineage-18.1-$1-nightly-oneplus3-signed.zip?sha256
 shasum -c $HOME/$LOS_FILENAME.1.zip;  # Not a zip file, it is the SHA256 hash
 aria2c https://dl.twrp.me/oneplus3/twrp-3.4.0-0-oneplus3.img.sha256
 sudo shasum -c $HOME/$TWRP_FILENAME.img.sha256;
-md5sum -c $HOME/$OGA_FILENAME.zip.md5;
-echo "079bc3a7f67413cb12509d1b811958dd7e7c7fb593d3f0507954fea72077b524 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
-shasum -c $HOME/$MAG_FILENAME.apk.sha256;
 
 echo "Removing checksum files...";
-rm $HOME/$LOS_FILENAME.1.zip $HOME/$TWRP_FILENAME.img.sha256 $HOME/$OGA_FILENAME.zip.md5 $HOME/$MAG_FILENAME.apk.sha256;
+rm $HOME/$LOS_FILENAME.1.zip $HOME/$TWRP_FILENAME.img.sha256;
 
 echo "Changing permissions..."
 chmod 660 $HOME/$LOS_FILENAME.zip;
@@ -47,18 +39,10 @@ sudo chgrp sdcard_rw $HOME/$LOS_FILENAME.zip;
 chmod 660 $HOME/$TWRP_FILENAME.img;
 sudo chown root $HOME/$TWRP_FILENAME.img;
 sudo chgrp sdcard_rw $HOME/$TWRP_FILENAME.img;
-chmod 660 $HOME/$OGA_FILENAME.zip;
-sudo chown root $HOME/$OGA_FILENAME.zip;
-sudo chgrp sdcard_rw $HOME/$OGA_FILENAME.zip;
-chmod 660 $HOME/$MAG_FILENAME.apk;
-sudo chown root $HOME/$MAG_FILENAME.apk;
-sudo chgrp sdcard_rw $HOME/$MAG_FILENAME.apk;
 
 echo "Moving files...";
 sudo mv $HOME/$LOS_FILENAME.zip /sdcard/;
 sudo mv $HOME/$TWRP_FILENAME.img /sdcard/;
-sudo mv $HOME/$OGA_FILENAME.zip /sdcard/;
-sudo mv $HOME/$MAG_FILENAME.apk /sdcard/$MAG_FILENAME.zip;
 
 echo "Script completed!";
 echo "DO NOT use Open GApps and Magisk, download MindTheGapps and Magisk separately...";
--- losup/scripts/op6t_dl_newest_LOS.sh
+++ losup/scripts/op6t_dl_newest_LOS.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 echo;
-echo "Sample usage: <script> <datestamp of LineageOS build> <datestamp of OpenGApps build>";
+echo "Sample usage: <script> <datestamp of LineageOS build>";
 echo 'Make sure to have run "pkg install aria2 perl tsu" beforehand!';
 echo "Press any key once all aria2c commands finish!";
 echo;
@@ -10,32 +10,24 @@ read;
 echo "Downloading files...";
 pushd $HOME;
 aria2c --force-sequential=true --max-connection-per-server=5 --split=10 \
-    https://mirrorbits.lineageos.org/full/fajita/$1/lineage-18.1-$1-nightly-fajita-signed.zip \
-    https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-11.0-nano-$2.zip \
-    https://downloads.sourceforge.net/project/opengapps/arm64/$2/open_gapps-arm64-11.0-nano-$2.zip.md5 \
-    https://downloads.sourceforge.net/project/mauronofrio-twrp/Fajita/twrp-3.3.1-32-fajita-installer-mauronofrio.zip \
-    https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk
+    https://mirrorbits.lineageos.org/full/fajita/$1/lineage-19.1-$1-nightly-fajita-signed.zip \
+    https://dl.twrp.me/fajita/twrp-installer-3.6.2_9-0-fajita.zip;
 popd;
 echo "Press any key once all aria2c instances have completed successfully, else press Ctrl-C to abort.";
 read;
 
 # Set variable names
-LOS_FILENAME="lineage-18.1-$1-nightly-fajita-signed";
-TWRP_FILENAME="twrp-3.3.1-32-fajita-installer-mauronofrio";
-OGA_FILENAME="open_gapps-arm64-11.0-nano-$2";
-MAG_FILENAME="Magisk-v23.0";
+LOS_FILENAME="lineage-19.1-$1-nightly-fajita-signed";
+TWRP_FILENAME="twrp-installer-3.6.2_9-0-fajita";
 
 echo "Verifying checksums...";
-aria2c https://mirrorbits.lineageos.org/full/fajita/$1/lineage-18.1-$1-nightly-fajita-signed.zip?sha256
+aria2c https://mirrorbits.lineageos.org/full/fajita/$1/lineage-19.1-$1-nightly-fajita-signed.zip?sha256
 shasum -c $HOME/$LOS_FILENAME.1.zip;  # Not a zip file, it is the SHA256 hash
-echo "e6c36b0a9ddc092e03038e5a4178ed8ce2206089e1d1067b0b0af10ba51ccfa2 *$TWRP_FILENAME.zip" > $HOME/$TWRP_FILENAME.zip.sha256;
+echo "b88383ab79f4e7d31e58ed44f5bdcfe3f7ab1639f87188b514af315c794de8af *$TWRP_FILENAME.zip" > $HOME/$TWRP_FILENAME.zip.sha256;
 shasum -c $HOME/$TWRP_FILENAME.zip.sha256;
-md5sum -c $HOME/$OGA_FILENAME.zip.md5;
-echo "079bc3a7f67413cb12509d1b811958dd7e7c7fb593d3f0507954fea72077b524 *$MAG_FILENAME.apk" > $HOME/$MAG_FILENAME.apk.sha256;
-shasum -c $HOME/$MAG_FILENAME.apk.sha256;
 
 echo "Removing checksum files...";
-rm $HOME/$LOS_FILENAME.1.zip $HOME/$TWRP_FILENAME.zip.sha256 $HOME/$OGA_FILENAME.zip.md5 $HOME/$MAG_FILENAME.apk.sha256;
+rm $HOME/$LOS_FILENAME.1.zip $HOME/$TWRP_FILENAME.zip.sha256;
 
 echo "Changing permissions..."
 chmod 660 $HOME/$LOS_FILENAME.zip;
@@ -44,18 +36,10 @@ sudo chgrp sdcard_rw $HOME/$LOS_FILENAME.zip;
 chmod 660 $HOME/$TWRP_FILENAME.zip;
 sudo chown root $HOME/$TWRP_FILENAME.zip;
 sudo chgrp sdcard_rw $HOME/$TWRP_FILENAME.zip;
-chmod 660 $HOME/$OGA_FILENAME.zip;
-sudo chown root $HOME/$OGA_FILENAME.zip;
-sudo chgrp sdcard_rw $HOME/$OGA_FILENAME.zip;
-chmod 660 $HOME/$MAG_FILENAME.apk;
-sudo chown root $HOME/$MAG_FILENAME.apk;
-sudo chgrp sdcard_rw $HOME/$MAG_FILENAME.apk;
 
 echo "Moving files...";
 sudo mv $HOME/$LOS_FILENAME.zip /sdcard/;
 sudo mv $HOME/$TWRP_FILENAME.zip /sdcard/;
-sudo mv $HOME/$OGA_FILENAME.zip /sdcard/;
-sudo mv $HOME/$MAG_FILENAME.apk /sdcard/$MAG_FILENAME.zip;
 
 echo "Script completed!";
 echo "DO NOT use Open GApps and Magisk, download MindTheGapps and Magisk separately...";
